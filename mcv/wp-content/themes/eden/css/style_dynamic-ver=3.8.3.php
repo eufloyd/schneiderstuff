593be7a86e3bd4308347942b3a23023fefaa61d2
Carlos Alberto Schneider <carlos.schneider@informant.com.br>
2014-06-26 02:00:49 -0300

    Iniciando criacao de html, comecando a remover os JSs específicos

---
+++ mcv/wp-content/themes/eden/css/style_dynamic-ver=3.8.3.php
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+.content{
+	margin-top: 0;
+}
+
+.content.content_top_margin{
+	margin-top: 85px !important;
+}
+
+
+
+
+	.touch section.parallax_section_holder{
+		height: auto !important;
+		min-height: 300px;  
+		background-position: center top !important;  
+		background-attachment: scroll;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	.woocommerce .content .content_inner > .container > .container_inner,
+	.woocommerce-cart .content .content_inner > .container > .container_inner,
+	.woocommerce-checkout .content .content_inner > .container > .container_inner,
+	.woocommerce-account .content .content_inner > .container > .container_inner{
+		padding-top:50px;
+	}
+	.woocommerce.single .content .content_inner > .container > .container_inner{
+		padding-top:135px;
+	}
+	.woocommerce.single .content .content_inner .title_outer{
+		display: none !important;
+		height: 0 !important;
+	}
+
+
